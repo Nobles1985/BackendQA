f75c0425ba6ae357113ebe6fefa229168a66b557
Denis Soroka <rabotden@gmail.com>
2022-12-23 17:11:24 +0300

    small fix

--- src/main/java/org/example/lesson4/dto/AddRequest.java
+++ src/main/java/org/example/lesson4/dto/AddRequest.java
@@ -3,7 +3,6 @@ package org.example.lesson4.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.Getter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
@@ -13,13 +12,10 @@ import lombok.Getter;
 })
 public class AddRequest {
     @JsonProperty("item")
-    @Getter
     private String item;
     @JsonProperty("aisle")
-    @Getter
     private String aisle;
     @JsonProperty("parse")
-    @Getter
     private Boolean parse;
 
     public AddRequest(String item, String aisle, Boolean parse){
